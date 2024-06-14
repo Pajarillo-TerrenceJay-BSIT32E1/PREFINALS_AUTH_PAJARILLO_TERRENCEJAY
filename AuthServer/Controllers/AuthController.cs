ed043256d090d04cc1c7e1b2e9d94323a38dbb20
Pajarillo-TerrenceJay-BSIT32E1 <161125385+Pajarillo-TerrenceJay-BSIT32E1@users.noreply.github.com>
2024-06-14 02:04:05 -0700

    Auth Folders

---
+++ AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuthServer.Core;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IAuthService _authService;
