353da050e57f24e5e270f9155da0c6fcd8c775f2
Pajarillo-TerrenceJay-BSIT32E1 <161125385+Pajarillo-TerrenceJay-BSIT32E1@users.noreply.github.com>
2024-06-14 01:48:30 -0700

    Add project files.

---
+++ PREFINALS_AUTH_PAJARILLO_TERRENCEJAY/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_PAJARILLO_TERRENCEJAY.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
