16f5864ca25666043b586f436e15da0800f1ad3f
Pajarillo-TerrenceJay-BSIT32E1 <161125385+Pajarillo-TerrenceJay-BSIT32E1@users.noreply.github.com>
2024-06-14 03:11:51 -0700

    AboutMe

--- ProtectedApi/Controllers/ValueController.cs
+++ ProtectedApi/Controllers/ValueController.cs
@@ -1,6 +1,25 @@
-﻿namespace ProtectedApi.Controllers
+﻿namespace ProtectedApi
 {
-    public class ValueController
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
     {
-    }
-}
+        private readonly string _owner = "Terrence Jay Pajarillo";
+
+        [HttpGet("about/me")]
+        public IActionResult AboutMe()
+        {
+            var random = new Random();
+            var thingsAboutOwner = new[]
+            {
+                "Im currently pursuing Bachelor of Science in Information Technology in Lyceum of Alabang",
+                "I love watching anime, playing online games and working on improving my design",
+                "I like to ride my motorcycle and and enjoy the exhilarating experience"
+            };
+            var thing = thingsAboutOwner[random.Next(thingsAboutOwner.Length)];
+
+            return Ok(thing);
+        }
